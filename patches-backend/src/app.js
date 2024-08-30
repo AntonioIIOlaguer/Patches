8e19a0f6cffe08e0412686c320351e0624149e75
AntonioIIOlaguer <antonio.olaguer.ii@gmail.com>
2024-08-30 13:56:37 -0400

    Add comments

--- patches-backend/src/app.js
+++ patches-backend/src/app.js
@@ -7,10 +7,14 @@ import logger from "./utils/logger.js";
 import mongoose from "mongoose";
 import usersRouter from "./controllers/usersRouter.js";
 
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+//Connect to DB
 mongoose.set("strictQuery", false);
-
 logger.info("connecting to", config.MONGODB_URI);
-
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
@@ -20,13 +24,11 @@ mongoose
     logger.error("error connecting to MongoDB:", error.message);
   });
 
-app.use(cors());
-app.use(express.static("dist"));
-app.use(express.json());
-app.use(middleware.requestLogger);
-
+//Establish routes
+app.use(middleware.tokenExtractor);
 app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
+
 export default app;
